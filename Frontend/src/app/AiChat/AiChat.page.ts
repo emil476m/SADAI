bc75ea0c2c27f58656d0b381db325fe63a08c599
TheDarkSide1992 <Jenscp171@gmail.com>
2024-03-08 11:31:22 +0100

    made frontend chat page

--- Frontend/src/app/AiChat/AiChat.page.ts
+++ Frontend/src/app/AiChat/AiChat.page.ts
@@ -1,12 +1,98 @@
-import { Component, inject, OnInit } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute} from "@angular/router";
+import {IonContent} from "@ionic/angular";
+import {FormControl, Validators} from "@angular/forms";
+import {Message} from "../models/Message.model";
 
 @Component({
   selector: 'app-AiChat',
-  templateUrl: './AiChat.page.html',
-  styleUrls: ['./AiChat.page.scss'],
-})
-export class AiChatPage{
+  template: `
+    <ion-header [translucent]="true">
+      <ion-toolbar>
+        <ion-title>
+          {{botName}}
+        </ion-title>
+      </ion-toolbar>
+    </ion-header>
 
-  constructor() {}
+    <ion-content [fullscreen]="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Blank</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-content #textWindow id="Textcontainer" [scrollEvents]="true">
+
+        <ion-card id="textCard" *ngFor="let message of messages"
+                  [ngClass]="{'left-card': !message.isUser, 'right-card': message.isUser}">
+          <ion-tab-bar [ngStyle]="{ 'background-color': message.isUser ? '#001087' : '#3A3B3C' }">
+            <ion-title style="color: White">{{ message.message }}</ion-title>
+          </ion-tab-bar>
+        </ion-card>
+      </ion-content>
+    </ion-content>
+    <ion-item>
+      <ion-input placeholder="  text...  " [formControl]="message" id="messageInput"></ion-input>
+      <ion-button (click)="sendMessage()" id="button" slot="end">
+        <ion-icon name="send-outline"></ion-icon>
+        <p>&#160; send message</p>
+      </ion-button>
+    </ion-item>
+  `,
+  styleUrls: ['AiChat.page.scss'],
+})
+export class AiChatPage implements OnInit {
+  constructor(private http: HttpClient) {
+  }
+
+  botName : string = "";
+
+  message: FormControl<string | null> = new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(50)]);
+
+  messages: Message[] = [];
+
+  ngOnInit() {
+    this.botName = "Gemini";
+
+
+    let text1: Message = {
+      message: "Hi I am " + this.botName + "\n I am a AI chat bot",
+      isUser: false,
+    }
+    let text2: Message = {
+      message: "How may I help you?",
+      isUser: false,
+    }
+
+
+    this.messages = [
+      text1,
+      text2
+    ];
+
+    this.getConnection();
+
+  }
+
+  async sendMessage() {
+    if (this.message.value != null) {
+      let text: Message = {
+        message: this.message.value,
+        isUser: true,
+      }
+
+      this.messages.push(text)
+
+      //TODO - Send Message
+    }
+
+  }
+
+
+  async getConnection() {
+    //ToDo esablish socket
+  }
+
 }
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {AiChatPage} from "./AiChat/AiChat.page";
 import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule],
+  declarations: [AppComponent, AiChatPage],
+  imports: [ReactiveFormsModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
