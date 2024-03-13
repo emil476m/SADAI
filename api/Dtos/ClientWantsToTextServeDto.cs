f3725f8f1d162a585726695bc3d9469c30144a28
TheDarkSide1992 <Jenscp171@gmail.com>
2024-03-13 09:00:13 +0100

    created model and model folder

---
+++ api/Dtos/ClientWantsToTextServeDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using lib;
+
+namespace socketAPIFirst.Dtos;
+
+public class ClientWantsToTextServeDto : BaseDto
+{
+    [MinLength(1)]
+    public string message { get; set; }
+    public bool isUser { get; set; }
+}
