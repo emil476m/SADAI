b58490ddd1012f8863dc4cb58322b140450471ed
andreas14101 <59689350+andreas14101@users.noreply.github.com>
2024-03-15 13:06:39 +0100

    Test filter (#4)
    * content filter method
    
    * merged init (not finished)
    
    * test for filter
    
    * changed name of of variable
    
    ---------
    
    Co-authored-by: hjorthen1243 <114651351+hjorthen1243@users.noreply.github.com>
    Co-authored-by: TheDarkSide1992 <Jenscp171@gmail.com>

--- api/ClientRequest/ClientWantsToTranslateText.cs
+++ api/ClientRequest/ClientWantsToTranslateText.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.Text.Json;
+using api.Filter;
 using Azure;
 using Azure.AI.Translation.Text;
 using Fleck;
@@ -12,32 +13,28 @@ public class ClientWantsToTranslateText : BaseEventHandler<ClientWantsToTranslat
 {
     public override async Task Handle(ClientWantsToTranslateTextDto dto, IWebSocketConnection socket)
     {
-        try
+        ContentFilter filter = new ContentFilter();
+        await filter.checkText(dto.text);
+
+        var lan = StateService.languages[socket.ConnectionInfo.Id];
+        var to = lan[dto.toLan];
+        var from = lan[dto.fromLan];
+        string apikey = Environment.GetEnvironmentVariable("tapikey");
+        string region = Environment.GetEnvironmentVariable("tregion");
+        TextTranslationClient ttc = new TextTranslationClient(new AzureKeyCredential(apikey), region);
+
+        Response<IReadOnlyList<TranslatedTextItem>> response =
+            await ttc.TranslateAsync(to, dto.text, from).ConfigureAwait(false);
+        IReadOnlyList<TranslatedTextItem> translations = response.Value;
+        TranslatedTextItem translation = translations.FirstOrDefault();
+
+        var echo = new ServerRespondsToUser()
         {
-            var lan = StateService.languages[socket.ConnectionInfo.Id];
-            var to = lan[dto.toLan];
-            var from = lan[dto.fromLan];
-            string apikey = Environment.GetEnvironmentVariable("tapikey");
-            string region = Environment.GetEnvironmentVariable("tregion");
-            TextTranslationClient ttc = new TextTranslationClient(new AzureKeyCredential(apikey), region);
+            message = translation?.Translations?.FirstOrDefault()?.Text,
+            isUser = false
+        };
+        var messageToClient = JsonSerializer.Serialize(echo);
 
-            Response<IReadOnlyList<TranslatedTextItem>> response =
-                await ttc.TranslateAsync(to, dto.text, from).ConfigureAwait(false);
-            IReadOnlyList<TranslatedTextItem> translations = response.Value;
-            TranslatedTextItem translation = translations.FirstOrDefault();
-
-            var echo = new ServerRespondsToUser()
-            {
-                message = translation?.Translations?.FirstOrDefault()?.Text,
-                isUser = false
-            };
-            var messageToClient = JsonSerializer.Serialize(echo);
-
-            socket.Send(messageToClient);
-        }
-        catch (Exception e)
-        {
-            throw new Exception("Failed to translate please try again");
-        }
+        socket.Send(messageToClient);
     }
 }
---
+++ api/Filter/ContentFilter.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace api.Filter;
+
+public class ContentFilter
+{
+    private HttpClient http = new HttpClient();
+    public record RequestModel(string text, List<string> categories, string outputType)
+    {
+        public override string ToString()
+        {
+            return $"{{ text = {text}, categories = {categories}, outputType = {outputType} }}";
+        }
+    }
+
+    public async Task checkText(string message)
+    {
+        HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "https://toxisityfilter.cognitiveservices.azure.com/contentsafety/text:analyze?api-version=2023-10-01");
+        
+        request.Headers.Add("accept", "application/json");
+        request.Headers.Add("Ocp-Apim-Subscription-Key", Environment.GetEnvironmentVariable("ContentFilterKey"));
+
+        var req = new RequestModel(message, new List<string>
+            { "Hate", "Sexual", "SelfHarm", "Violence" }, "FourSeverityLevels");
+
+        request.Content = new StringContent(JsonSerializer.Serialize(req));
+        request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        
+        HttpResponseMessage response = await http.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+        var obj = JsonSerializer.Deserialize<ContentFilterResponce>(responseBody);
+
+        var isToxic = obj.categoriesAnalysis.Count(e => e.severity > 1) >= 1;
+
+        if (isToxic)
+        {
+            throw new ValidationException("Such language is not allowed");
+        }
+    }
+}
+
+public class CategoriesAnalysis
+{
+    public string category { get; set; }
+    public int severity { get; set; }
+}
+
+public class ContentFilterResponce
+{
+    public List<object> blocklistsMatch { get; set; }
+    public List<CategoriesAnalysis> categoriesAnalysis { get; set; }
+}
